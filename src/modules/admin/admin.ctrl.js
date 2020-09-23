3d5d559e008a151fb88c8b71e90789a5f220c28d
prasad.bamane <prasad.bamane@leadschool.in>
2020-09-24 00:26:30 +0530

    admin added

---
+++ src/modules/admin/admin.ctrl.js
@@ -0,0 +1,14 @@
+class Admin {
+
+    getAdmin() {
+        return {
+            first_name: "Dhananjay",
+            last_name: "Bhirud"
+        }
+    }
+
+    setAdmin(admin) {
+        this.admin.first_name = admin.first_name
+    }
+
+}
